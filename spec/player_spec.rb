399753e543463856eefcc8b901bb87aa057edc65
Liana Lo <liana.lo@prezi.com>
2014-12-06 13:02:57 +0100

    tests for future straight and straight flush (commented out for now)

--- spec/player_spec.rb
+++ spec/player_spec.rb
@@ -227,7 +227,7 @@ describe 'Player' do
           "current_buy_in":0,
           "pot":0
       }')
-      expect(player.bet_request(game_state)).to eq(55 * BET_MULTIPLIER)
+      expect(player.bet_request(game_state)).to eq(50 * BET_MULTIPLIER)
     end
 
     it 'should return 800 for a triple' do
@@ -282,7 +282,7 @@ describe 'Player' do
       expect(player.bet_request(game_state)).to eq(80 * BET_MULTIPLIER)
     end
 
-    it 'should return blah for a flush' do
+    it 'should return 1000 for a flush' do
       player = Player.new
       game_state = JSON.parse('{
           "players":[
@@ -333,6 +333,110 @@ describe 'Player' do
       }')
       expect(player.bet_request(game_state)).to eq(100 * BET_MULTIPLIER)
     end
+
+    # it 'should return blah for a straight flush' do
+    #   player = Player.new
+    #   game_state = JSON.parse('{
+    #       "players":[
+    #       {"name":"Player 1",
+    #       "stack":1000,
+    #       "status":"active",
+    #       "bet":0,
+    #       "hole_cards":[],
+    #       "version":"Version name 1",
+    #       "id":0
+    #   },
+    #       {"name":"Rubot",
+    #       "stack":1000,
+    #       "status":"active",
+    #       "bet":0,
+    #       "hole_cards":[
+    #         {
+    #           "rank": "4",
+    #           "suit": "spades"
+    #         },
+    #         {
+    #           "rank": "5",
+    #           "suit": "spades"
+    #         }
+    #       ],
+    #       "version":"Version name 2",
+    #       "id":1
+    #   }
+    #   ],
+    #       "small_blind":10,
+    #       "orbits":0,
+    #       "dealer":0,
+    #       "community_cards":[
+    #         {
+    #           "rank": "6",
+    #           "suit": "spades"
+    #         },
+    #         {
+    #           "rank": "7",
+    #           "suit": "spades"
+    #         },
+    #         {
+    #           "rank": "8",
+    #           "suit": "spades"
+    #         }],
+    #       "current_buy_in":0,
+    #       "pot":0
+    #   }')
+    #   expect(player.bet_request(game_state)).to eq(100 * BET_MULTIPLIER)
+    # end
+
+    # it 'should return blah for a straight' do
+    #   player = Player.new
+    #   game_state = JSON.parse('{
+    #       "players":[
+    #       {"name":"Player 1",
+    #       "stack":1000,
+    #       "status":"active",
+    #       "bet":0,
+    #       "hole_cards":[],
+    #       "version":"Version name 1",
+    #       "id":0
+    #   },
+    #       {"name":"Rubot",
+    #       "stack":1000,
+    #       "status":"active",
+    #       "bet":0,
+    #       "hole_cards":[
+    #         {
+    #           "rank": "4",
+    #           "suit": "hearts"
+    #         },
+    #         {
+    #           "rank": "5",
+    #           "suit": "spades"
+    #         }
+    #       ],
+    #       "version":"Version name 2",
+    #       "id":1
+    #   }
+    #   ],
+    #       "small_blind":10,
+    #       "orbits":0,
+    #       "dealer":0,
+    #       "community_cards":[
+    #         {
+    #           "rank": "6",
+    #           "suit": "hearts"
+    #         },
+    #         {
+    #           "rank": "7",
+    #           "suit": "spades"
+    #         },
+    #         {
+    #           "rank": "8",
+    #           "suit": "spades"
+    #         }],
+    #       "current_buy_in":0,
+    #       "pot":0
+    #   }')
+    #   expect(player.bet_request(game_state)).to eq(80 * BET_MULTIPLIER)
+    # end
 
   end
 end
