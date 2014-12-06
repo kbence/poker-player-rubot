e1690476f0a9adfe1f74cbeeab7be133d10c6f33
Bence Kiglics <bence.kiglics@prezi.com>
2014-12-06 12:44:20 +0100

    migrated tests to new rspec format

--- spec/ranking_spec.rb
+++ spec/ranking_spec.rb
@@ -16,31 +16,31 @@ end
 describe 'Ranking' do
   describe 'rank' do
     it 'should return 0 for no hands' do
-      Ranking.new.rank(cards('S4', 'C6')).should be(0)
+      expect(Ranking.new.rank(cards('S4', 'C6'))).to eq(0)
     end
 
     it 'should return 10 for high cards' do
-      Ranking.new.rank(cards('CA', 'DJ')).should be(10)
+      expect(Ranking.new.rank(cards('CA', 'DJ'))).to eq(10)
     end
 
     it 'should return 20 for pair' do
-      Ranking.new.rank(cards('C9', 'D9')).should be(20)
+      expect(Ranking.new.rank(cards('C9', 'D9'))).to eq(20)
     end
 
     it 'should return 30 for high pair' do
-      Ranking.new.rank(cards('C10', 'D10')).should be(30)
+      expect(Ranking.new.rank(cards('C10', 'D10'))).to eq(30)
     end
 
     it 'should return 40 for two pairs' do
-      Ranking.new.rank(cards('C9', 'D9', 'S5', 'H5')).should be(60)
+      expect(Ranking.new.rank(cards('C9', 'D9', 'S5', 'H5'))).to eq(60)
     end
 
     it 'should return 50 for three of a kind' do
-      Ranking.new.rank(cards('C4', 'D4', 'D3', 'S4')).should be(50)
+      expect(Ranking.new.rank(cards('C4', 'D4', 'D3', 'S4'))).to eq(50)
     end
 
     it 'should return 30 in case of flush chance' do
-      Ranking.new.rank(cards('S5', 'S2', 'S3', 'D4', 'D6')).should be(30)
+      expect(Ranking.new.rank(cards('S5', 'S2', 'S3', 'D4', 'D6'))).to eq(30)
     end
 
     it 'should return 40 in case of almost flush' do
