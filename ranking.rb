4509dd0b532d714da284ce2b49be3e1c5d9d66bc
Bence Kiglics <bence.kiglics@prezi.com>
2014-12-06 11:31:12 +0100

    made some refactoring

--- ranking.rb
+++ ranking.rb
@@ -2,14 +2,24 @@
 class Ranking
   def rank(cards)
     value = 0
-    cards.each do |card|
-      rank = card['rank']
-
-      if %w(A Q K J).include? rank
-        value += 5
-      end
-    end
+    value += rank_cards(cards)
 
     value
+  end
+
+  def rank_cards(cards)
+    sum(cards.map do |card|
+          rank = card['rank']
+
+          if %w(A Q K J).include? rank
+            5
+          else
+            0
+          end
+        end)
+  end
+
+  def sum(array)
+    array.inject{|sum,x| sum + x}
   end
 end
