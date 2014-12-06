d9142de4fbbb387edd104fb96b79b29636aa2eb2
Liana Lo <liana.lo@prezi.com>
2014-12-06 12:56:08 +0100

    test for flush

--- spec/player_spec.rb
+++ spec/player_spec.rb
@@ -230,7 +230,7 @@ describe 'Player' do
       expect(player.bet_request(game_state)).to eq(55 * BET_MULTIPLIER)
     end
 
-    it 'should return 550 for a triple' do
+    it 'should return 800 for a triple' do
       player = Player.new
       game_state = JSON.parse('{
           "players":[
@@ -280,6 +280,58 @@ describe 'Player' do
           "pot":0
       }')
       expect(player.bet_request(game_state)).to eq(80 * BET_MULTIPLIER)
+    end
+
+    it 'should return blah for a flush' do
+      player = Player.new
+      game_state = JSON.parse('{
+          "players":[
+          {"name":"Player 1",
+          "stack":1000,
+          "status":"active",
+          "bet":0,
+          "hole_cards":[],
+          "version":"Version name 1",
+          "id":0
+      },
+          {"name":"Rubot",
+          "stack":1000,
+          "status":"active",
+          "bet":0,
+          "hole_cards":[
+            {
+              "rank": "4",
+              "suit": "spades"
+            },
+            {
+              "rank": "5",
+              "suit": "spades"
+            }
+          ],
+          "version":"Version name 2",
+          "id":1
+      }
+      ],
+          "small_blind":10,
+          "orbits":0,
+          "dealer":0,
+          "community_cards":[
+            {
+              "rank": "9",
+              "suit": "spades"
+            },
+            {
+              "rank": "4",
+              "suit": "spades"
+            },
+            {
+              "rank": "4",
+              "suit": "spades"
+            }],
+          "current_buy_in":0,
+          "pot":0
+      }')
+      expect(player.bet_request(game_state)).to eq(100 * BET_MULTIPLIER)
     end
 
   end
